c3369a963f6b79da39eb7fae30116394195bc279
ihnatsen <ivanihnatsenkau030202@gmail.com>
2025-03-26 11:50:55 +0100

    New model of database. New diagram of database.

--- data_base/console_create_warsaw_park_data_base.sql
+++ data_base/console_create_warsaw_park_data_base.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS park
     address     text,
     rating      float,
     district_id int not null,
-    foreign key (district_id) REFERENCES district(district_id)
+    foreign key (district_id) REFERENCES district (district_id)
 );
 
 -- Districts of Warsaw.
@@ -16,4 +16,31 @@ CREATE TABLE IF NOT EXISTS district
 (
     district_id INT primary key,
     name        text
+);
+
+-- District of park.
+CREATE TABLE IF NOT EXISTS district_of_park
+(
+    district_id int,
+    park_id     int,
+    primary key (park_id, district_id),
+    foreign key (district_id) REFERENCES district (district_id),
+    foreign key (park_id) REFERENCES park (park_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS street
+(
+    street_id   int primary key,
+    street_name text
+);
+
+CREATE TABLE IF NOT EXISTS street_of_park
+(
+    street_id int,
+    park_id   int,
+    primary key (park_id, street_id),
+    foreign key (street_id) REFERENCES street (street_id),
+    foreign key (park_id) REFERENCES park (park_id)
+
 );
